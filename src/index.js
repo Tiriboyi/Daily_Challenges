22b0ace6380973882b311fafbc6f95231cc276b6
Fortune Tiriboyi <tiriboyifortune@gmail.com>
2023-08-08 18:09:07 +0200

    Added new tests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 //module.js:
-import { MissingRange } from './missingrange.js';
+import { MissingRange } from './missingRange.js';
 
 console.log(MissingRange([1, 2, 3, 4, 5, 10, 11, 12]));
--- test/test.js
+++ test/test.js
@@ -1,40 +1,25 @@
-import assert from 'assert';
-import {MissingRange} from '../src/missingrange.js';
+import { expect } from 'chai';
+import {MissingRange} from '../src/missingRange.js';
 
+describe('Find Missing Ranges', function(){
 
-describe('MissingRange', () => {
-    it('should handle an empty array', () => {
-        const nums = [];
-        const result = MissingRange(nums);
-        const expected = ['0->99'];
-        assert.deepStrictEqual(result, expected);
-    });
+ 
 
-    it('should handle a single missing number at the beginning', () => {
-        const nums = [1, 2, 3, 4, 5, 10, 11, 12];
-        const result = MissingRange(nums);
-        const expected = ['0', '6->9', '13->99'];
-        assert.deepStrictEqual(result, expected);
-    });
+    // Arrange
+    const tests = [
+        {ranges: [1,3,4,9], expected: ['0', '2', '5->8', '10->99']},
+        {ranges: [], expected: ['0->99']},
+        {ranges: [99], expected: ['0->98']},
+        {ranges: [0], expected: ['1->99']},
+        {ranges: [9], expected: ['0->8','10->99']}
+    ];
 
-    it('should handle a single missing number at the end', () => {
-        const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        const result = MissingRange(nums);
-        const expected = ['11->99'];
-        assert.deepStrictEqual(result, expected);
-    });
-
-    it('should handle multiple missing ranges', () => {
-        const nums = [2, 5, 7, 10, 15, 20, 21];
-        const result = MissingRange(nums);
-        const expected = ['0->1', '3->4', '6', '8->9', '11->14', '16->19', '22->99'];
-        assert.deepStrictEqual(result, expected);
-    });
-
-    it('should handle no missing ranges', () => {
-        const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        const result = MissingRange(nums);
-        const expected = ['10->99'];
-        assert.deepStrictEqual(result, expected);
+    tests.forEach(function({ranges, expected}) {
+        it(`It should take [${ranges.join(',')}] and return expected results`, function(){
+            // Act
+            let result = MissingRange(ranges);
+            // Assert
+            expect(result).to.eql(expected);
+        })
     });
 });
