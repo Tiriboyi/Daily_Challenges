d219728bd62a9778163fa4486f5348a69f0eae38
Fortune Tiriboyi <tiriboyifortune@gmail.com>
2023-09-11 11:09:37 +0200

    new updates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 //module.js:
 import { MissingRange } from './missingRange.js';
+import RomanNumeralsToInt from './romannumeralstoint.js';
+import isHeteromecic from './pronic.js';
 
-console.log(MissingRange([1, 2, 3, 4, 5, 10, 11, 12]));
+// console.log(MissingRange([1, 2, 3, 4, 5, 10, 11, 12]));
+
+// console.log(RomanNumeralsToInt('VVV'));
+
+console.log(isHeteromecic(0));
+// console.log(isHeteromecic(1));
+// console.log(isHeteromecic(2));
+// console.log(isHeteromecic(7));
+// console.log(isHeteromecic(136));
+// console.log(isHeteromecic(156));
